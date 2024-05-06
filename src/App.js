d8e892d9c8f0451c74591fc3f678917e21dfcf50
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-05-06 17:55:52 +0300

    Implemented complete functionality for this mini app, including movie search, display, and handling empty search results.
    Implemented complete functionality for this mini app, including movie search, display, and handling empty search results.

--- src/App.js
+++ src/App.js
@@ -5,19 +5,15 @@ import { useEffect, useState } from 'react';
 
 const API_URL = "http://www.omdbapi.com?apikey=40b1c31a";
 
-const movie1 = {
-  "Title": "Amazing Spiderman Syndrome",
-  "Year": "2012",
-  "imdbID": "tt2586634",
-  "Type": "movie",
-  "Poster": "N/A"
-}
-
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);
+
+    setMovies(data.Search);
   }
 
   useEffect(() => {
@@ -30,21 +26,32 @@ const App = () => {
       <div className='search'>
         <input
           placeholder="Search for movies"
-          value="Superman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
         <img
           src={SearchIcon}
           alt="Search"
-          // onChange={() => {}}
-        />
-      </div>
-      <div className="container">
-        <MovieCard movie1={movie1}
-
+          onClick={() => searchMovies(searchTerm)}
         />
       </div>
 
+      {
+        movies?.length > 0
+          ? (
+            <>
+              <div className="container">
+                {movies.map((movie) => (
+                  <MovieCard movie={movie} />
+                ))}
+              </div>
+            </>
+          ) : (
+            <div className='empty'>
+              <h2>No Movies Found</h2>
+            </div>
+          )
+      }
     </div>
   )
 }
