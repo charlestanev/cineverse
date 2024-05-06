017eda4a78142baf2c45712cc78a1618443fa116
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-05-06 17:26:42 +0300

    adding the css design, search icon, API and MovieCard component
    adding the css design, search icon, API and MovieCard component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import MovieCard from './MovieCard';
+import SearchIcon from './search.svg';
+import { useEffect, useState } from 'react';
 
-function App() {
+const API_URL = "http://www.omdbapi.com?apikey=40b1c31a";
+
+const movie1 = {
+  "Title": "Amazing Spiderman Syndrome",
+  "Year": "2012",
+  "imdbID": "tt2586634",
+  "Type": "movie",
+  "Poster": "N/A"
+}
+
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('Spiderman');
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Cineverce</h1>
+      <div className='search'>
+        <input
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img
+          src={SearchIcon}
+          alt="Search"
+          // onChange={() => {}}
+        />
+      </div>
+      <div className="container">
+        <MovieCard movie1={movie1}
+
+        />
+      </div>
+
     </div>
-  );
+  )
 }
 
 export default App;
