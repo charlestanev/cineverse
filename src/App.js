256b6f834fb21fcb18e587fe53d763e255c41b6a
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-03-12 11:23:06 +0200

    Add Babel plugins for private properties and update MovieCard component with key prop

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const App = () => {
             <>
               <div className="container">
                 {movies.map((movie) => (
-                  <MovieCard movie={movie} />
+                  <MovieCard key={movie.imdbID} movie={movie} />
                 ))}
               </div>
             </>
